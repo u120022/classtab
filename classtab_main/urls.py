6102a038cd971c212c459c8742af849dbd496327
u120022 <u120022@st.pu-toyama.ac.jp>
2022-12-28 14:27:13 +0000

    impl clazz table oneshot
    授業テーブルのoneshotなデータ取得用のフォームと表示ページを追加
    それに従ってパッケージu120022/webclass-parserを追加

--- classtab_main/urls.py
+++ classtab_main/urls.py
@@ -2,4 +2,16 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path(
+        "form_clazz_table_oneshot",
+        views.form_clazz_table_oneshot,
+        name="form_clazz_table_oneshot",
+    ),
+    path(
+        "show_clazz_table_oneshot",
+        views.show_clazz_table_oneshot,
+        name="show_clazz_table_oneshot",
+    ),
+]
